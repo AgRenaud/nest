9054930a267ea47508d81d083abd20e17eba87ab
AgRenaud <renaud.aguirrechambilla@gmail.com>
2023-07-20 22:09:33 +0200

    add login page

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,5 +1,24 @@
-use axum::response::Html;
+use maud::{DOCTYPE, Markup, html};
 
-pub async fn home() -> Html<&'static str> {
-    Html("<h1>Welcome to Nest ! 🪺</h1>")
+pub async fn home() -> Markup {
+    html!(
+        (DOCTYPE)
+        head {
+            meta charset="utf-8";
+            script src="https://unpkg.com/htmx.org@1.9.3" {};
+            title { "Nest" }
+        }
+        body {
+            h1 { "Welcome to nest ! 🪺"}
+            form hx-post= "/login" {
+                label for="uname" { b { "Username" } }
+                input type="text" placeholder="Enter Username" name="uname" required;                
+            
+                label for="psw" { b { "Password" } }
+                input type="password" placeholder="Enter Password" name="psw" required;                
+            
+                button type="submit" { "Login" }
+            }
+        }
+    )
 }
