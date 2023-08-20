21625f4e8ebf028b065d40d6ddd5e477239128a0
AgRenaud <renaud.aguirrechambilla@gmail.com>
2023-08-21 00:28:07 +0200

    add user to db (clear password)

--- migrations/20230801113620_add-user-schemas.up.sql
+++ migrations/20230801113620_add-user-schemas.up.sql
@@ -1,9 +1,12 @@
 -- Add up migration script here
 
+CREATE TYPE user_roles AS ENUM ('admin','contributor');
+
+
 CREATE TABLE users (
     username CITEXT NOT NULL UNIQUE,
-    name VARCHAR(100) NOT NULL,
     password VARCHAR(128) NOT NULL,
+    role user_roles NOT NULL DEFAULT 'contributor',
 
     CONSTRAINT users_len_username CHECK (length(username) <= 50),
     CONSTRAINT users_valid_username CHECK (username ~* '^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$')
